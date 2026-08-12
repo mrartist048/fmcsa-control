ecdd86e2fa9e665efa77bb8b9c36c70107b0bbcd
mrartist048 <89958173+mrartist048@users.noreply.github.com>
2026-08-13 01:51:35 +0500

    Update scraper-core.js

--- scraper-core.js
+++ scraper-core.js
@@ -21,7 +21,7 @@ const allowedUsers = {
     "precisionx": { pass: "precisionx123", maxLaptops: 1, expires: "2026-07-30", dbUrl: FIREBASE_DB_URL_1 },  
     "dispatchloadify": { pass: "admin789", maxLaptops: 5, expires: "2026-09-01", dbUrl: FIREBASE_DB_URL_1 }, 
     "baitstarlogistics": { pass: "baitstarlogistics123", maxLaptops: 10, expires: "2026-08-30", dbUrl: FIREBASE_DB_URL_2 }, // Project 2
-    "testinguser": { pass: "testinguser123", maxLaptops: 5, expires: "2026-08-30", dbUrl: FIREBASE_DB_URL_2 },          // Project 2
+    "testinguser": { pass: "testinguser123", maxLaptops: 2, expires: "2026-08-30", dbUrl: FIREBASE_DB_URL_2 },          // Project 2
     "Skylinelogistics": { pass: "Skylinelogistics123", maxLaptops: 2, expires: "2026-08-30", dbUrl: FIREBASE_DB_URL_1 },  
     "Loadlink": { pass: "Loadlink#trial", maxLaptops: 1, expires: "2026-08-14", dbUrl: FIREBASE_DB_URL_2 },
 };
